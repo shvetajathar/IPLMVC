dea6d1dfa7e3fd33727558179a9636ff65a302a0
Iamneo <techadmin@iamneo.ai>
2023-11-03 09:50:12 +0000

    Backup Commit

--- dotnetapp/Models/Player.cs
+++ dotnetapp/Models/Player.cs
@@ -7,6 +7,10 @@ namespace dotnetapp.Models
 {
     public class Player
     {
-        // Write your Player class here...        
+        public int Id{get;set;}
+        public string Name{get;set;}
+        public int Age{get;set;}
+        public string Category{get;set;}
+        public decimal BiddingPrice{get;set;}       
     }
 }
--- dotnetapp/Models/Team.cs
+++ dotnetapp/Models/Team.cs
@@ -7,6 +7,11 @@ namespace dotnetapp.Models
 {
     public class Team
     {
+         
+        public string TeamName{get;set;}
+        public int Age{get;set;}
+        public string Category{get;set;}
+        public decimal BiddingPrice{get;set;}
         // Write your Team class here...        
     }
 }
