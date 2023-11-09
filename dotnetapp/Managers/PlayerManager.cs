bf6fbdce40763534345b877969bf26e9e7fec05c
Iamneo <techadmin@iamneo.ai>
2023-11-09 12:38:33 +0000

    Backup Commit

--- dotnetapp/Managers/PlayerManager.cs
+++ dotnetapp/Managers/PlayerManager.cs
@@ -16,15 +16,15 @@ namespace dotnetapp.Managers
         
         public void DisplayAllPlayers()
         {
-            string constr="User ID =sa;password=examlyMssql@123;server=localhost;Database=DbName;trusted_connection=false;Persist Security Info=False;Encrypt=False";
-            SqlConnection con=new SqlConnection(constr);
-            SqlCommand cmd=new SqlCommand("select * from Player",con);
-            con.Open();
-            SqlDataReader reader=cmd.ExecuteReader();
-            while(reader.Read())
-            {
-                console.WriteLine($"{reader["Id"]} {reader["Name"]} {reader["Age"]} {reader["Category"]} {reader["BiddingAmount"]}");
-            }
+            // string constr="User ID =sa;password=examlyMssql@123;server=localhost;Database=DbName;trusted_connection=false;Persist Security Info=False;Encrypt=False";
+            // SqlConnection con=new SqlConnection(constr);
+            // SqlCommand cmd=new SqlCommand("select * from Player",con);
+            // con.Open();
+            // SqlDataReader reader=cmd.ExecuteReader();
+            // while(reader.Read())
+            // {
+            //     console.WriteLine($"{reader["Id"]} {reader["Name"]} {reader["Age"]} {reader["Category"]} {reader["BiddingAmount"]}");
+            // }
 
         }
         public void AddPlayer(Player p)
@@ -58,22 +58,22 @@ namespace dotnetapp.Managers
 
         public void AddPlayerToDatabase()
         {
-            using (SqlConnection connection = new SqlConnection(connectionString))
-            {
-                connection.Open();
-                string query = "INSERT INTO Player VALUES (@Id, @Name, @Age, @Category,@BiddingAmount)";
+            // using (SqlConnection connection = new SqlConnection(connectionString))
+            // {
+            //     connection.Open();
+            //     string query = "INSERT INTO Player VALUES (@Id, @Name, @Age, @Category,@BiddingAmount)";
 
-                using (SqlCommand command = new SqlCommand(query, connection))
-                {
-                    command.Parameters.AddWithValue("@Id", Player.Id);
-                    command.Parameters.AddWithValue("@Name", Player.Name);
-                    command.Parameters.AddWithValue("@Age", Player.Age);
-                    command.Parameters.AddWithValue("@Category", Player.Category);
-                    command.Parameters.AddWithValue("@BiddingAmount", Player.BiddingAmount);
+            //     using (SqlCommand command = new SqlCommand(query, connection))
+            //     {
+            //         command.Parameters.AddWithValue("@Id", Player.Id);
+            //         command.Parameters.AddWithValue("@Name", Player.Name);
+            //         command.Parameters.AddWithValue("@Age", Player.Age);
+            //         command.Parameters.AddWithValue("@Category", Player.Category);
+            //         command.Parameters.AddWithValue("@BiddingAmount", Player.BiddingAmount);
                     
-                    return (int)command.ExecuteScalar();
-                }
-            }
+            //         return (int)command.ExecuteScalar();
+            //     }
+            // }
         }
         
         // Write your fuctions here...
